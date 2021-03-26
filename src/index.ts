eec415811fb56941d3042f58b92447e528fc366e
Leoyang6612 <forever1597753@gmail.com>
2021-03-26 21:47:39 +0800

    First Commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import m from "mithril";
+
+const btn = m("button", {style:{color: "red"}}, "CLICK ME")
+m.render(document.body, btn);
+
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
+    },
+    plugins: [new HtmlWebpackPlugin(
+        {
+            title: 'Custom template',
+        }
+    )],
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: "ts-loader" }
+        ]
+    }
+};
