a7c87ba705d5d6ac69c553faf58b6123e246d393
Tkjm <thomasandyjk@gmail.com>
2021-04-11 19:15:14 +0800

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,10 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: 'index_bundle.js',
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        compress: false,
+    },
     plugins: [new HtmlWebpackPlugin(
         {
             title: 'Custom template',
